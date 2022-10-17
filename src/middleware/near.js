ac7646a7d52b5eea5adb1c097a34ca2033f6b7ab
Den Ilin <followdarko@gmail.com>
2022-10-17 21:05:04 +0300

    Add ftmetadata endpoint (#23)
    * Add ftmetadata endpoint
    
    * Tuned getFtMetadata token handler. Added withNear midleware for indexer.
    
    * Added tests for token ft_metadata
    
    * Tuned tokens ft_metadata endpoint
    
    * Refactored for more readable way
    
    * Tuned namings and fixed type

--- src/middleware/near.js
+++ src/middleware/near.js
@@ -179,12 +179,18 @@ const withNear = async (ctx, next) => {
     await next();
 };
 
+const initViewAccount = async (ctx, next) => {
+    ctx.nearViewAccount = await ctx.near.account('dontcare');
+    await next();
+};
+
 module.exports = {
     parseSeedPhrase: require('near-seed-phrase').parseSeedPhrase,
     creatorKeyJson,
     creatorKeysJson,
     fundedCreatorKeyJson,
     withNear,
+    initViewAccount,
     checkAccountOwnership,
     createCheckAccountDoesNotExistMiddleware,
     accountAuthMiddleware,
